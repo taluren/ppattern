5a998cf6326996d158aa6b86306103fbcabcd844
vialette <vialette@gmail.com>
2016-12-11 19:41:08 +0100

    Add Generator#randomBinaryString

--- src/data/algorithm/ppattern/Generator.hs
+++ src/data/algorithm/ppattern/Generator.hs
@@ -13,10 +13,20 @@ commentary with @some markup@.
 module Data.Algorithm.PPattern.Generator
 (
   random123Avoiding
+, randomBinaryString
 )
 where
+
+  import qualified Data.List        as L
+  import qualified System.Random    as Random
+  import qualified System.IO.Unsafe as Unsafe
 
   import Data.Algorithm.PPattern.Types
 
   random123Avoiding :: Length -> Permutation
   random123Avoiding n = [1..n]
+
+  randomBinaryString :: Length -> [Int]
+  randomBinaryString n = L.take n $ Random.randomRs (0,1) $ g
+    where
+      g = Unsafe.unsafePerformIO newStdGen
