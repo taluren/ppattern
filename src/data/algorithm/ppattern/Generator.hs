de879c704ff696276c0d4a8fede3b4e44e4e529f
vialette <vialette@gmail.com>
2016-12-11 18:20:36 +0100

    add src/data/algorithm/ppattern/Generator.hs

---
+++ src/data/algorithm/ppattern/Generator.hs
@@ -0,0 +1,19 @@
+{-|
+Module      : Data.Algorithm.PPattern.Generator
+Description : Short description
+Copyright   : (c) Stéphane Vialette, 2016
+License     : MIT
+Maintainer  : vialette@gmail.com
+Stability   : experimental
+
+Here is a longer description of this module, containing some
+commentary with @some markup@.
+-}
+
+module Data.Algorithm.PPattern.Generator
+(
+  generate
+)
+where
+
+  generate = id
